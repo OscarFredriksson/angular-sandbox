328b9c8d7760cbff97b82a91249e22953f16162d
atemmel <temmeladam@gmail.com>
2020-06-27 22:35:18 +0000

    Changed dimensions, introduced bars between chapters

--- apps/sidebar-toc/src/app/pages/start-page/start-page.component.ts
+++ apps/sidebar-toc/src/app/pages/start-page/start-page.component.ts
@@ -9,9 +9,12 @@ export class StartPageComponent implements OnInit
 {
 
   sections = [
-    { id: "s1", title: "Section 1" },
-    { id: "s2", title: "Section 2" },
-    { id: "s3", title: "Section 3" }
+    { id: "s1", title: "Chapter title" },
+    { id: "s2", title: "Another Chapter title" },
+    { id: "s3", title: "Another cool title" },
+	//TODO: Make sure the component works with longer titles
+	//{ id: "s3", title: "Another very cool and profound chapter title" },
+	{ id: "s4", title: "Closing words" }
   ]
 
   constructor () { }
